5537103ae8e3e19a74af2b40b0f7df105c21e3e1
Spencer Boles <48936680+spencerboles@users.noreply.github.com>
2022-07-24 10:58:14 -0600

    migrate models to sqlite3 database

--- lifeapp/budget/models.py
+++ lifeapp/budget/models.py
@@ -1,17 +1,18 @@
+from datetime import datetime
 from pydoc import describe
 from django.db import models
-from django.forms import BooleanField, CharField, DateTimeField, FloatField 
-import datetime
+from django.forms import BooleanField, CharField, DateTimeField, FloatField, Textarea, DecimalField 
+from django.utils.timezone import now
 
 # Create your models here. 
 
 class Item(models.Model): 
     title = CharField(max_length=150) 
-    description = TextField(max_length=300) 
-    amount = FloatField()
-    date_incurred = DateTimeField(default=datetime.now()) 
+    description = Textarea() 
+    amount = DecimalField(max_digits=10, decimal_places=2) 
+    date_incurred = models.DateTimeField(default=datetime.now) 
     bucket = models.ForeignKey("Bucket", on_delete=models.CASCADE) 
-    isRevenue = BooleanField() 
+    is_revenue = BooleanField() 
     
 
     def __str__(self): 
@@ -19,7 +20,18 @@ class Item(models.Model):
 
 class Bucket(models.Model): 
     title = CharField(max_length=150) 
-    description = TextField(max_length=300) 
-    is_budget = BooleanField()
+    description = Textarea() 
+    is_budget = BooleanField() 
+    account = models.ForeignKey("Account", on_delete=models.CASCADE) 
+
+    def __Str__(self): 
+        return f'{self.title}' 
+
+class Account(models.Model): 
+    title = CharField(max_length=150)
+    description = Textarea() 
+    balance = DecimalField(max_digits=10, decimal_places=2) 
+    
+
 
 
